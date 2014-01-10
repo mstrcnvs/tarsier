67bbba40fc1df36e0a06ee78aeb99d86a4c8102c
Rafael Canovas <rafaelcanovas@me.com>
2014-01-09 22:40:55 -0200

    Basic byte serving funcionality

--- media/__init__.py
+++ media/__init__.py
@@ -2,7 +2,9 @@
 
 from __future__ import absolute_import
 
-from flask import Blueprint, render_template
+import re
+
+from flask import Blueprint, Response, request, render_template
 from media.utils import find_media, find_media_or_404
 
 media = Blueprint('media', __name__, template_folder='templates')
@@ -25,4 +27,43 @@ def watch(hash):
 def stream(hash):
 	media = find_media_or_404(hash=hash)
 
-	return ''
+	stream = False
+	initial = 0
+	final = media.size - 1
+
+	if 'Range' in request.headers:
+		stream = True
+
+		ranges = re.findall(r'\d+', request.headers['Range'])
+		initial = int(ranges[0])
+
+		if len(ranges) > 1:
+			final = int(ranges[1])
+
+	with open(media.full_path, 'rb') as file:
+		if stream:
+			file.seek(initial)
+
+		data = file.read(final - initial)
+
+	response = Response(
+		data,
+		status='200',
+		headers={
+			'Content-Type': media.mimetype,
+			'Content-Length': str(final - initial),
+			'Content-Transfer-Encoding': 'binary',
+			'Accept-Ranges': 'bytes',
+		},
+		mimetype=media.mimetype
+	)
+
+	if stream:
+		response.status = '206'
+		response.headers['Content-Range'] = '%s-%s/%s' % (
+			initial,
+			final,
+			final - initial
+		)
+
+	return response
