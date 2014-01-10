355400605f784c95adbc6b1438253feca7b3ef9e
Rafael Canovas <rafaelcanovas@me.com>
2014-01-09 22:05:10 -0200

    MediaFile now has mimetype and size attributes

--- media/utils.py
+++ media/utils.py
@@ -6,13 +6,34 @@ from hashlib import sha1
 from flask import current_app, abort
 
 
+class MediaFile(object):
+	def __init__(self, root, name):
+		self.root = root
+		self.name = name
+		self.full_path = os.path.join(root, name)
+		self.hash = sha1(self.full_path.encode('utf-8')).hexdigest()[:8]
+
+		_, ext = os.path.splitext(name)
+		if ext == '.ogg':
+			self.mimetype = 'video/ogg'
+		elif ext == '.mp4':
+			self.mimetype = 'video/mp4'
+
+	@property
+	def size(self):
+		"""
+		Return the size in bytes of the given media file
+		"""
+		return os.path.getsize(self.full_path)
+
+
 def find_media(hash=None):
 	folder = current_app.config['MEDIA_FOLDER'].decode('utf-8')
 	files = [
 		MediaFile(root, file)
 		for root, dirs, files in os.walk(folder)
 		for file in files
-		if os.path.splitext(file)[-1] in ['.avi', '.mp4']
+		if os.path.splitext(file)[-1] in ['.ogg', '.mp4']
 	]
 
 	if hash:
@@ -27,11 +48,3 @@ def find_media(hash=None):
 
 def find_media_or_404(hash):
 	return find_media(hash=hash) or abort(404)
-
-
-class MediaFile(object):
-	def __init__(self, root, name):
-		self.root = root
-		self.name = name
-		self.full_path = os.path.join(root, name)
-		self.hash = sha1(self.full_path.encode('utf-8')).hexdigest()[:8]
